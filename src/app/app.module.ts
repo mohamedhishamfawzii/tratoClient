d473e55ad2b6feab8a93fb0b711f20e144b58ffb
hisham <m.hisham@live.com>
2018-02-21 23:45:31 +0200

    ya moshyl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,32 @@ import { NavComponent } from './nav/nav.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { SigninComponent } from './signin/signin.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { MenuComponent } from './menu/menu.component';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { CatComponent } from './cat/cat.component';
+import { SelectComponent } from './select/select.component';
+import { MainComponent } from './main/main.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     CarouselComponent,
-    SigninComponent
+    SigninComponent,
+    SearchbarComponent,
+    MenuComponent,
+    CatComponent,
+    SelectComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule
     , NgZorroAntdModule.forRoot(), FormsModule ,
-    ReactiveFormsModule
+    ReactiveFormsModule,BrowserAnimationsModule,NoopAnimationsModule,MatSelectModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
